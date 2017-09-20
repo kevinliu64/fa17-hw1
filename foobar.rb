f135c3bd4bbcf7c15a0abfc2b607c0137b331e31
kevinliu64 <kevinliu36@berkeley.edu>
2017-09-20 16:02:27 -0700

    hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,13 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    temp = a.map {|item| item.to_i}.map {|item| item + 2}
+    temp = temp.uniq.select {|item| (item.even?) && (item < 10)}
+    counter = 0
+    temp.each do |item|
+      counter += item
+    end
+    return counter
   end
 end
 
